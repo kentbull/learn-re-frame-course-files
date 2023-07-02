92223b34683e2869d6c1affba3a9dfccda62c01b
Kent Bull <kent@kentbull.com>
2023-07-02 16:30:03 -0600

    Add in Smooth UI components and re-frame

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,15 +1,24 @@
 (ns app.core
-  (:require [reagent.core :as r]
-            [reagent.dom :as rd]))
+  (:require ;[reagent.core :as r]
+   [reagent.dom :as rd]
+   [re-frame.core :as rf]
+   [app.db]
+   [app.theme :refer [cheffy-theme]]
+   ["@smooth-ui/core-sc" :refer [Normalize Button ThemeProvider]]))
 
 (defn app
   []
-  [:div "Cheffy"])
+  [:<>
+   [:> Normalize]
+   [:> ThemeProvider {:theme cheffy-theme}
+    [:> Button {:variant "primary"} "hello"]
+    [:div "Cheffy"]]])
 
 (defn ^:dev/after-load start
   []
+  (rf/dispatch-sync [:initialize-db])
   (rd/render [app]
-    (.getElementById js/document "app")))
+             (.getElementById js/document "app")))
 
 (defn ^:export init
   []
